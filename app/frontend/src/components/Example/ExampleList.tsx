3657b59ce8fbed708da38a4623d81ef54f26099a
Jeremiah Lipscomb <t-jelipscomb@microsoft.com>
2023-05-30 16:43:33 -0700

    Removed README.

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What is .NET?",
+        value: "What is .NET?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What are SDL Scripts?", value: "What are SDL Scripts?" },
+    { text: "How to add performance testing to a pipeline?", value: "How to add performance testing to a pipeline?" }
 ];
 
 interface Props {
--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -134,9 +134,9 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
-                            <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
+                            {/*ADD A LOGO*/}
+                            <h1 className={styles.chatEmptyStateTitle}>Chat with documentation</h1>
+                            <h2 className={styles.chatEmptyStateSubtitle}>Enter a question or click an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} />
                         </div>
                     ) : (
@@ -181,7 +181,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. What is .NET?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
